ec256df93af2d4f80428eca4a72e7fa9efb5e689
Chance Magno and Xi Xia <chance@>
2017-02-20 11:45:14 -0800

    add loop to simplify

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,35 @@
 $(document).ready(function() {
   $("#formOne").submit(function() {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput= $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var feelingInput = $("input#feeling").val();
-    var videoGameInput = $("input#videoGame").val();
+    var array = document.getElementsByTagName('label');
+    var onlyForValues =[];
+      for (i=0; i < array.length; i++) {
+        onlyForValues [i] = array[i].getAttribute("for")
+      };
+      debugger;
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".feeling").text(feelingInput);
-    $(".videoGame").text(videoGameInput);
+      onlyForValues.forEach(function(output){
+        var answer  = $("input#"+ output).val();
+      $("." + output).text(answer);
+      })
+
+    // [0].getAttribute("for");
+    // console.log(array);
+
+    // var person1Input = $("input#person1").val();
+    // var person2Input = $("input#person2").val();
+    // var animalInput= $("input#animal").val();
+    // var exclamationInput = $("input#exclamation").val();
+    // var verbInput = $("input#verb").val();
+    // var feelingInput = $("input#feeling").val();
+    // var videoGameInput = $("input#videoGame").val();
+    //
+    // $(".person1").text(person1Input);
+    // $(".person2").text(person2Input);
+    // $(".animal").text(animalInput);
+    // $(".exclamation").text(exclamationInput);
+    // $(".verb").text(verbInput);
+    // $(".feeling").text(feelingInput);
+    // $(".videoGame").text(videoGameInput);
     $("#story").show();
 
     event.preventDefault();
